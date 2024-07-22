68e15bc6e4e7f6f8ab7c33e5e5b831ba1542c4a5
Swastik Bhattacharyya <swastikunity@gmail.com>
2024-07-22 21:01:19 +0530

    Add functions to serialize and deserialize BKTree to and from a file
    This commit adds the rkyv crate for serialization. The BKTree and Node
    structs in the bk_tree module have been updated with the traits required
    to create their archives required for this function. The usize
    properties have been changed to fixed sized integer properties to create
    the archives.
    
    In addition, two functions from_file() and save_to_file() has been added
    for deserializing and serializing the struct respectively.

--- src/bk_tree.rs
+++ src/bk_tree.rs
@@ -1,7 +1,13 @@
-#[derive(Clone, Debug)]
+use std::{fs::File, io::{BufReader, BufWriter, Read, Write}};
+use rkyv::{AlignedVec, Archive, Deserialize, Serialize};
+
+#[derive(Clone, Debug, Archive, Serialize, Deserialize, PartialEq)]
+#[archive(compare(PartialEq), check_bytes)]
+#[archive_attr(derive(Debug))]
+#[readonly::make]
 pub struct Node {
     pub word: String,
-    pub next: Vec<Option<usize>>
+    pub next: Vec<Option<u32>>
 }
 
 impl Node {
@@ -13,22 +19,39 @@ impl Node {
     }
 }
 
-#[derive(Debug)]
+#[derive(Archive, Serialize, Deserialize, Debug, PartialEq)]
+#[archive(compare(PartialEq), check_bytes)]
+#[archive_attr(derive(Debug))]
 #[readonly::make]
 pub struct BKTree {
-    pub max_word_length: usize,
-    pub alphabet_length: usize,
+    pub max_word_length: u16,
+    pub alphabet_length: u16,
     pub tree: Vec<Node>,
-    pub size: usize
+    pub size: u32
 }
 
 impl BKTree {
-    pub fn new(max_word_length: usize, alphabet_length: usize, max_words: usize) -> Self {
+    pub fn new(max_word_length: u16, alphabet_length: u16, max_words: usize) -> Self {
         Self {
             max_word_length,
             alphabet_length,
-            tree: vec![Node::new("".to_string(), max_word_length); max_words],
+            tree: vec![Node::new("".to_string(), max_word_length as usize); max_words],
             size: 0
+        }
+    }
+
+    pub fn from_file(file_path: &str) -> std::io::Result<Self> {
+        let mut file: File = std::fs::File::open(file_path)?;
+        let mut reader: BufReader<&mut File> = BufReader::new(&mut file);        
+        
+        let mut bytes: Vec<u8> = Vec::new();
+        reader.read_to_end(&mut bytes)?;
+
+        let tree_result = rkyv::from_bytes::<BKTree>(&bytes);
+
+        match tree_result {
+            Ok(tree) => Ok(tree),
+            Err(_) => Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "Failed to deserialize BKTree"))
         }
     }
 
@@ -50,7 +73,7 @@ impl BKTree {
             dp[0][j + 1] = infinity;
         }
 
-        let mut da: Vec<usize> = vec![0; self.alphabet_length];
+        let mut da: Vec<usize> = vec![0; self.alphabet_length as usize];
 
         for i in 1..=m {
             let mut db: usize = 0;
@@ -89,11 +112,16 @@ impl BKTree {
                 Some(0) => return,
                 Some(d) if self.tree[current].next[d].is_none() => {
                     if !self.tree[current].word.is_empty() { self.tree[current].next[d] = Some(self.size); }
-                    self.tree[self.size].word = word;
+                    self.tree[self.size as usize].word = word;
                     self.size += 1;
                     return;
                 },
-                Some(d) => current = self.tree[current].next[d].unwrap(),
+                Some(d) => {
+                    match self.tree[current].next[d] {
+                        Some(n) => current = n as usize,
+                        None => return
+                    }
+                }
                 None => return
             }
         }
@@ -109,7 +137,12 @@ impl BKTree {
             match distance {
                 Some(0) => return true,
                 Some(d) if self.tree[current].next[d].is_none() => return false,
-                Some(d) => current = self.tree[current].next[d].unwrap(),
+                Some(d) => {
+                    match self.tree[current].next[d] {
+                        Some(n) => current = n as usize,
+                        None => return false
+                    }
+                },
                 None => return false
             }
         }
@@ -132,15 +165,32 @@ impl BKTree {
 
             for i in tolerance_start..=tolerance_end {
                 if self.tree[current].next[i].is_some() {
-                    stack.push(self.tree[current].next[i]?);
+                    stack.push(self.tree[current].next[i]? as usize);
                 }
             }
         }
 
         Some(result)
     }
-}
 
+    pub fn save_to_file(&self, file_path: &str) -> std::io::Result<()> {
+        let bytes: AlignedVec = match rkyv::to_bytes::<_, 256>(self) {
+            Ok(b) => b,
+            Err(_) => return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "Failed to serialize BKTree"))
+        };
+        let file: File = match std::fs::File::create(file_path) {
+            Ok(f) => f,
+            Err(e) => return Err(e)
+        };
+        let mut writer: BufWriter<File> = BufWriter::new(file);
+        match writer.write_all(&bytes) {
+            Ok(_) => (),
+            Err(e) => return Err(e)
+        }
+
+        Ok(())
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -148,13 +198,13 @@ mod tests {
     use super::super::dictionary::Dictionary;
 
     #[test]
-    #[ignore = "Computationally Expensive"]
+    #[ignore = "Computationally expensive since it loads the entire dictionary"]
     fn test_from_dictionary() {
         let dictionary: Dictionary = Dictionary::from_file("dictionary.txt", 255).unwrap();
-        let mut tree: BKTree = BKTree::new(dictionary.max_word_length, dictionary.alphabet_length, dictionary.words.len());
+        let mut tree: BKTree = BKTree::new(dictionary.max_word_length as u16, dictionary.alphabet_length as u16, dictionary.words.len());
 
-        assert_eq!(tree.alphabet_length, dictionary.alphabet_length);
-        assert_eq!(tree.max_word_length, dictionary.max_word_length);
+        assert_eq!(tree.alphabet_length as usize, dictionary.alphabet_length);
+        assert_eq!(tree.max_word_length as usize, dictionary.max_word_length);
 
         for word in dictionary.words.iter() {
             tree.add(word.clone());
@@ -181,5 +231,41 @@ mod tests {
         assert!(similar_words.contains(&"hella".to_string()));
         assert!(similar_words.contains(&"hell".to_string()));
         assert!(similar_words.contains(&"help".to_string()));
+    }
+
+    #[test]
+    fn test_file_serialization() {
+        let mut tree: BKTree = BKTree::new(3, 255, 5);
+
+        tree.add("hello".to_string());
+        tree.add("world".to_string());
+        tree.add("hella".to_string());
+        tree.add("hell".to_string());
+        tree.add("help".to_string());
+
+        tree.save_to_file("bk_tree.bin").unwrap();
+
+        let new_tree: BKTree = BKTree::from_file("bk_tree.bin").unwrap();
+        
+        assert_eq!(tree, new_tree);
+        std::fs::remove_file("bk_tree.bin").unwrap();
+    }
+
+    #[test]
+    #[ignore = "Computationally expensive since it loads the entire dictionary"]
+    fn test_full_file_serialization() {
+        let dictionary: Dictionary = Dictionary::from_file("dictionary.txt", 255).unwrap();
+        let mut tree: BKTree = BKTree::new(dictionary.max_word_length as u16, dictionary.alphabet_length as u16, dictionary.words.len());
+
+        for word in dictionary.words.iter() {
+            tree.add(word.clone());
+        }
+
+        tree.save_to_file("bk_tree_full.bin").unwrap();
+
+        let new_tree: BKTree = BKTree::from_file("bk_tree_full.bin").unwrap();
+ 
+        assert_eq!(tree, new_tree);
+        std::fs::remove_file("bk_tree_full.bin").unwrap();
     }
 }
