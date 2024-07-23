43f6dc36617f4fbf86ab766d1a2c82fb33852c1f
TechnoFlask <technoflask03@gmail.com>
2024-07-23 11:06:44 +0530

    Add module bloom_filter and utils for first level filtering
    This commit adds the module bloom_filter that contains the BloomFilter
    struct along with its implementations. It also adds utils module that
    contains necessary utility functions for hashing, serialization and
    deserialization. The BloomFilter struct can be used to perform initial
    filtering to optimize spell checkers.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+pub mod bloom_filter;
+pub mod utils;
+
+use bloom_filter::BloomFilter;
+
 fn main() {
-    println!("Hello, world!");
+    let mut bf = BloomFilter::new(20, 0.05);
+    let word_present = vec![
+        "A", "quick", "brown", "Fox", "jUmps", "over", "A", "lazy", "DOG",
+    ];
+
+    for word in word_present.iter() {
+        bf.insert(word);
+    }
+
+    let test = [
+        "never", "brown", "hello", "die", "what??", "Fox", "GOD", "over",
+    ];
+
+    for w in test.iter() {
+        if bf.lookup(w) {
+            println!("{} is probably present", w);
+        } else {
+            println!("{} is definitely absent....", w);
+        }
+    }
+
+    println!();
+
+    bf.to_file("bf.bin").unwrap();
+    let new_bf = BloomFilter::from_file("bf.bin").unwrap();
+    assert_eq!(bf, new_bf);
+
+    for w in test.iter() {
+        if new_bf.lookup(w) {
+            println!("{} is probably present", w);
+        } else {
+            println!("{} is definitely absent....", w);
+        }
+    }
 }
---
+++ src/utils.rs
@@ -0,0 +1,61 @@
+use std::hash::{DefaultHasher, Hash, Hasher};
+
+use rkyv::{
+    de::deserializers::{SharedDeserializeMap, SharedDeserializeMapError},
+    ser::serializers::{
+        AlignedSerializer, AllocScratch, AllocScratchError, CompositeSerializer,
+        CompositeSerializerError, FallbackScratch, HeapScratch, SharedSerializeMap,
+        SharedSerializeMapError,
+    },
+    validation::{
+        validators::{CheckDeserializeError, DefaultValidator, DefaultValidatorError},
+        CheckArchiveError,
+    },
+    AlignedVec, Archive, Deserialize, Serialize,
+};
+
+pub fn hash_with_seed(input: &str, seed: u32) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    hasher.write_u32(seed);
+    input.hash(&mut hasher);
+
+    return hasher.finish();
+}
+
+pub fn serialize<'a, T>(
+    value: &T,
+) -> Result<
+    AlignedVec,
+    CompositeSerializerError<std::convert::Infallible, AllocScratchError, SharedSerializeMapError>,
+>
+where
+    T: Serialize<
+        CompositeSerializer<
+            AlignedSerializer<AlignedVec>,
+            FallbackScratch<HeapScratch<256>, AllocScratch>,
+            SharedSerializeMap,
+        >,
+    >,
+{
+    rkyv::to_bytes::<T, 256>(value)
+}
+
+pub fn deserialize<'a, T: Archive>(
+    bytes: &'a [u8],
+) -> Result<
+    T,
+    CheckDeserializeError<
+        CheckArchiveError<
+            <<T as Archive>::Archived as rkyv::CheckBytes<DefaultValidator<'_>>>::Error,
+            DefaultValidatorError,
+        >,
+        SharedDeserializeMapError,
+    >,
+>
+where
+    <T as Archive>::Archived: rkyv::CheckBytes<DefaultValidator<'a>>,
+    <T as Archive>::Archived: Deserialize<T, SharedDeserializeMap>,
+    <T as Archive>::Archived: 'a,
+{
+    rkyv::from_bytes::<T>(bytes)
+}
