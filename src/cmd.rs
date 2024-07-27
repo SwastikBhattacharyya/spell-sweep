8c09dbba71ed011548552d0ccd962c339c4e3674
TechnoFlask <technoflask03@gmail.com>
2024-07-27 18:12:55 +0530

    Add module cmd
    This commit adds module cmd that contains code for cmd args parsing,
    input data parsing..

---
+++ src/cmd.rs
@@ -0,0 +1,103 @@
+use std::{
+    error::Error,
+    fs,
+    io::{self, Read},
+    path::PathBuf,
+    process,
+};
+
+use clap::{command, value_parser, Arg};
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+enum Data {
+    File(PathBuf),
+    Pipe(Option<String>),
+}
+
+fn read_stdin() -> Result<Option<String>> {
+    if !atty::is(atty::Stream::Stdin) {
+        let stdin = io::stdin();
+        let mut reader = stdin.lock();
+
+        let mut buffer = Vec::<u8>::new();
+        reader.read_to_end(&mut buffer)?;
+
+        return Ok(Some(
+            String::from_utf8_lossy(buffer.as_slice())
+                .trim()
+                .to_string(),
+        ));
+    }
+
+    Ok(None)
+}
+
+fn handle_input_data(input: Data) -> Result<String> {
+    match input {
+        Data::File(file_path) => handle_file(file_path),
+        Data::Pipe(piped_data) => Ok(handle_piped_data(piped_data)),
+    }
+}
+
+fn handle_file(file_path: PathBuf) -> Result<String> {
+    let mut file = fs::File::open(file_path)?;
+
+    let mut buffer = Vec::<u8>::new();
+
+    file.read_to_end(&mut buffer)?;
+
+    Ok(String::from_utf8_lossy(buffer.as_slice())
+        .trim()
+        .to_string())
+}
+
+fn handle_piped_data(piped_data: Option<String>) -> String {
+    match piped_data {
+        None => {
+            eprintln!("Error: Provide file path or pipe some data in.");
+            process::exit(1);
+        }
+        Some(data) => data,
+    }
+}
+
+pub fn parse_cmd_args() -> Result<()> {
+    let matches = command!()
+        .arg(
+            Arg::new("filepath")
+                .short('f')
+                .long("file")
+                .help("Path to the source file")
+                .value_parser(value_parser!(PathBuf)),
+        )
+        .get_matches();
+
+    let data = match matches.get_one::<PathBuf>("filepath") {
+        Some(file_path) => handle_input_data(Data::File(file_path.to_path_buf()))?,
+        None => handle_input_data(Data::Pipe(read_stdin()?))?,
+    };
+
+    println!("{}", data);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::process::Command;
+
+    // use super::*;
+    #[test]
+    fn file_not_found() {
+        let output = Command::new("cargo")
+            .arg("run")
+            .arg("--")
+            .arg("--file")
+            .arg("/home/technoflask/.bashr")
+            .output()
+            .expect("Failed to execute command");
+
+        assert_eq!(output.status.success(), false);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-mod bloom_filter;
 mod bk_tree;
+mod bloom_filter;
+mod cmd;
 mod dictionary;
 mod processor;
 mod spell_check;
@@ -13,6 +14,11 @@ fn main() {
     let dictionary_path: &str = "dictionary.txt";
     let alphabet_length: u16 = 255;
 
-    let spell_check: SpellCheck = SpellCheck::new(bk_tree_path, bloom_filter_path, dictionary_path, alphabet_length);
+    let spell_check: SpellCheck = SpellCheck::new(
+        bk_tree_path,
+        bloom_filter_path,
+        dictionary_path,
+        alphabet_length,
+    );
     spell_check.run();
 }
